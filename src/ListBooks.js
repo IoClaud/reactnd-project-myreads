1a2106b089d91990bcec4fee4815de861e4c9f7e
ioClaud <claudio.gavi@gmail.com>
2018-07-01 11:20:21 +0200

    feat: add core books mapping for categories

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -2,52 +2,32 @@ import React, { Component } from 'react'
 
 class ListBooks extends Component {
   render() {
-    console.log('Props', this.props)
+    //console.log('Props', this.props)
+    const {book} = this.props
     return (
-      <div className="list-books">
-        <div className="list-books-title">
-          <h1>MyReads</h1>
-        </div>
-        <div className="list-books-content">
-          <div>
-            <div className="bookshelf">
+      <div className="book">
+        <div className="book-top">
+          <div className="book-cover" style={{
+            width: `${book.width}`+'px',
+            height: `${book.height}`+'px',
+            backgroundImage: `url(${book.thumbnail})`
+          }}/>
 
-              <h2 className="bookshelf-title">Currently Reading</h2>
-
-              <div className="bookshelf-books">
-                <ol className="books-grid">
-                  {this.props.books.map((book) => (
-                    <li key = {book.id}>
-                    <div className="book">
-                      <div className="book-top">
-                        <div className="book-cover" style={{
-                          width: `${book.width}`+'px',
-                          height: `${book.height}`+'px',
-                          backgroundImage: `url(${book.thumbnail})`
-                        }}/>
-
-                        <div className="book-shelf-changer">
-                          <select>
-                            <option value="move" disabled>Move to...</option>
-                            <option value="currentlyReading">Currently Reading</option>
-                            <option value="wantToRead">Want to Read</option>
-                            <option value="read">Read</option>
-                            <option value="none">None</option>
-                          </select>
-                        </div>
-                      </div>
-                      <div className="book-title">{book.title}</div>
-                      <div className="book-authors">{book.author}</div>
-                    </div>
-                    </li>
-                  ))}
-
-                </ol>
-              </div>
-            </div>
+          <div className="book-shelf-changer">
+            <select>
+              <option value="move" disabled>Move to...</option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+              <option value="none">None</option>
+            </select>
           </div>
         </div>
+        <div className="book-title">{book.title}</div>
+        <div className="book-authors">{book.author}</div>
       </div>
+
+
     )
   }
 }
---
+++ src/ListCategories.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import ListBooks from './ListBooks'
+
+class ListCategories extends Component {
+  render() {
+    console.log('ListCategories',this.props)
+    return (
+      <div>
+        {this.props.categories.map((category) => (
+            <div className="bookshelf" key={category.id}>
+              <h2 className="bookshelf-title">{category.title}</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                  {this.props.books.filter(book => book.category === category.id).map((book) => (
+                    <li key = {book.id}>
+                      <ListBooks
+                        books = {this.props.books}
+                        book = {book}
+                      />
+                    </li>
+                  ))}
+                </ol>
+              </div>
+            </div>
+        ))}
+      </div>
+    )
+  }
+}
+
+export default ListCategories
